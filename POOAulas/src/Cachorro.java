30c37c4eff2814436580b455bb2c4651bf74f55e
TASS� HELEN DO NASCIMENTO LAZAME <tassy.7214@aluno.pr.senac.br>
2024-08-02 19:47:11 -0300

    primeiro commit da pasta para o git

---
+++ POOAulas/src/Cachorro.java
@@ -0,0 +1,20 @@
+
+public class Cachorro {
+	
+	private String cor;
+	private double tamanho;
+	private char sexo;
+	private boolean statusVida;
+	
+	public void mover() {
+		System.out.println("to andando");
+	}
+	
+	public void latir() {
+		System.out.println("fazendo barulho");
+	}
+	
+	public void dormir() {
+		System.out.println("mimirrr");
+	}
+}
---
+++ POOAulas/src/Testar01.java
@@ -0,0 +1,18 @@
+
+public class Testar01 {
+
+	public static void main(String[] args) {
+
+		Cachorro c = new Cachorro();
+		
+		Moto m = new Moto();
+		
+
+		
+		m.mostrarInformacoes();
+		System.out.println("---------------");
+		m.acelerar();
+		
+	}
+
+}
